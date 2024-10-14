352248d17d23742cde76c09d7d630e0fa60d86a2
Ben Giles <ben.giles@wmcompliance.co.uk>
2024-10-14 13:39:01 +0100

    Add project files.

---
+++ BensBlazorWebAssembly/Data/Context.cs
@@ -0,0 +1,38 @@
+﻿using BensBlazorWebAssembly.Common.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Net.NetworkInformation;
+
+namespace BensBlazorWebAssembly.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VideoGame>().HasData(GetGames());
+        }
+
+        public DbSet<VideoGame> Games { get; set; }
+
+        /// <summary>
+        /// Get a list of games
+        /// as this does not create an instance of the class, it is static
+        /// </summary>
+        /// <returns></returns>
+        private static IEnumerable<VideoGame> GetGames()
+        {
+            var listOfGames = new List<VideoGame>();
+            listOfGames.Add(new VideoGame { Id = 1, Title = "Halo", Genre = "Shooter", Platform = Common.Enums.Platform.XboxOne, AgeRestriction = Common.Enums.AgeRestriction.Eighteen });
+            listOfGames.Add(new VideoGame { Id = 2, Title = "Mario", Genre = "Platformer", Platform = Common.Enums.Platform.NintendoSwitch, AgeRestriction = Common.Enums.AgeRestriction.Three });
+            listOfGames.Add(new VideoGame { Id = 3, Title = "God of War", Genre = "Action", Platform = Common.Enums.Platform.PS4, AgeRestriction = Common.Enums.AgeRestriction.Eighteen });
+            listOfGames.Add(new VideoGame { Id = 4, Title = "Fortnite", Genre = "Battle Royale", Platform = Common.Enums.Platform.Android, AgeRestriction = Common.Enums.AgeRestriction.Twelve });
+            listOfGames.Add(new VideoGame { Id = 5, Title = "Minecraft", Genre = "Sandbox", Platform = Common.Enums.Platform.Windows, AgeRestriction = Common.Enums.AgeRestriction.Seven });
+            return listOfGames;
+        }
+    }
+}
---
+++ BensBlazorWebAssembly/Program.cs
@@ -0,0 +1,38 @@
+using BensBlazorWebAssembly.Components;
+using BensBlazorWebAssembly.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+// Register the Db context
+builder.Services.AddDbContext<Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BensBlazorWebAssembly.Client._Imports).Assembly);
+
+app.Run();
